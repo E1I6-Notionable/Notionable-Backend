c569fc0dccbf899df0a968b97a12b4dd1c82e783
seokhee <skdlzl753@naver.com>
2023-11-15 10:33:40 +0900

    init

---
+++ src/main/java/com/e1i6/notionable/NotionableApplication.java
@@ -0,0 +1,12 @@
+package com.e1i6.notionable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionableApplication.class, args);
+	}
+}
---
+++ src/test/java/com/e1i6/notionable/NotionableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e1i6.notionable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
