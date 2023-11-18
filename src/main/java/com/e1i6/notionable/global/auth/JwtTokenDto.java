29457de542531aaa8b30a5c65556087929b72c26
seokhee <skdlzl753@naver.com>
2023-11-18 19:19:19 +0900

    user email login

--- src/main/java/com/e1i6/notionable/global/auth/JwtTokenDto.java
+++
@@ -1,1 +0,0 @@
-package com.e1i6.notionable.global.auth;import java.util.Date;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;@Builder@Data@AllArgsConstructorpublic class JwtTokenDto {	private String grantType;	private String accessToken;	private String refreshToken;	private Date expirationDate;}
