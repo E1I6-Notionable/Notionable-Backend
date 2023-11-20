b126565b80e2bfac56096b62ff7133f87b596ce2
bbangjun <lucid9dawn@gmail.com>
2023-11-20 16:59:59 +0900

    feat(CoolSMSController): api key 및 api secret 암호화

--- src/main/java/com/e1i6/notionable/coolsms_auth/controller/CoolSMSController.java
+++ src/main/java/com/e1i6/notionable/coolsms_auth/controller/CoolSMSController.java
@@ -2,16 +2,20 @@ package com.e1i6.notionable.coolsms_auth.controller;
 
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.service.DefaultMessageService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoolSMSController {
 
     private final DefaultMessageService messageService;
+    private final Environment env;
 
-    public CoolSMSController () {
-        this.messageService = NurigoApp.INSTANCE.initialize("발급받은 API key",
-                "발급받은 SECRET key", "https://api.coolsms.co.kr");
+    public CoolSMSController (Environment env) {
+        String api_key = env.getProperty("cool.sms.api.key");
+        String api_secret = env.getProperty("cool.sms.api.secret");
+        this.env = env;
+        this.messageService = NurigoApp.INSTANCE.initialize(api_key,
+                api_secret, "https://api.coolsms.co.kr");
     }
 }
