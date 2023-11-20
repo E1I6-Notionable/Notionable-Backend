8ba8cce6db655b9be85913f2111828cb76fbd1c6
bbangjun <lucid9dawn@gmail.com>
2023-11-20 16:52:08 +0900

    feat(CoolSMSController): 인스턴스 스켈레톤 코드 작성

--- src/main/java/com/e1i6/notionable/coolsms_auth/controller/CoolSMSController.java
+++ src/main/java/com/e1i6/notionable/coolsms_auth/controller/CoolSMSController.java
@@ -1,8 +1,17 @@
 package com.e1i6.notionable.coolsms_auth.controller;
 
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CoolSMSController {
 
+    private final DefaultMessageService messageService;
+
+    public CoolSMSController () {
+        this.messageService = NurigoApp.INSTANCE.initialize("발급받은 API key",
+                "발급받은 SECRET key", "https://api.coolsms.co.kr");
+    }
 }
