98aa579eef8c78a9004b91d0822380603a044fb2
bbangjun <lucid9dawn@gmail.com>
2023-11-20 19:20:49 +0900

    refactor(CoolSMSService.java): MVC 패턴으로 문자 인증 구현 로직 리팩토링

--- src/main/java/com/e1i6/notionable/coolsms_auth/controller/CoolSMSController.java
+++ src/main/java/com/e1i6/notionable/coolsms_auth/controller/CoolSMSController.java
@@ -1,45 +1,24 @@
 package com.e1i6.notionable.coolsms_auth.controller;
 
 import com.e1i6.notionable.coolsms_auth.dto.CoolSMSDto;
-import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.model.Message;
+import com.e1i6.notionable.coolsms_auth.service.CoolSMSService;
+import lombok.RequiredArgsConstructor;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
-import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
-import net.nurigo.sdk.message.service.DefaultMessageService;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 public class CoolSMSController {
 
-    private final DefaultMessageService messageService;
-    private final Environment env;
-
-    public CoolSMSController (Environment env) {
-        String api_key = env.getProperty("cool.sms.api.key");
-        String api_secret = env.getProperty("cool.sms.api.secret");
-        this.env = env;
-        this.messageService = NurigoApp.INSTANCE.initialize(api_key,
-                api_secret, "https://api.coolsms.co.kr");
-    }
+    @Autowired
+    private CoolSMSService coolSMSService;
 
     @PostMapping("/users/signup/send-code")
     public SingleMessageSentResponse sendAuthCode(@RequestBody CoolSMSDto coolSMSDto) {
-        Random random = new Random();
-        Message message = new Message();
-        String sendNumber = env.getProperty("cool.sms.phone.number");
-        String authCode = String.valueOf(random.nextInt(8888)+1000); // 범위 : 1000 ~ 9999
 
-        message.setFrom(sendNumber);
-        message.setTo(coolSMSDto.getPhoneNumber());
-        message.setText("notionable 회원가입 인증번호는 [" + authCode + "]입니다.");
-
-        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-        System.out.println(response);
+        SingleMessageSentResponse response = coolSMSService.sendAuthCode(coolSMSDto);
 
         return response;
     }
--- src/main/java/com/e1i6/notionable/coolsms_auth/service/CoolSMSService.java
+++ src/main/java/com/e1i6/notionable/coolsms_auth/service/CoolSMSService.java
@@ -1,7 +1,44 @@
 package com.e1i6.notionable.coolsms_auth.service;
 
+import com.e1i6.notionable.coolsms_auth.dto.CoolSMSDto;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class CoolSMSService {
+
+    private final DefaultMessageService messageService;
+    private Environment env;
+
+    public CoolSMSService(Environment env) {
+        String api_key = env.getProperty("cool.sms.api.key");
+        String api_secret = env.getProperty("cool.sms.api.secret");
+        this.messageService = NurigoApp.INSTANCE.initialize(api_key,
+                api_secret, "https://api.coolsms.co.kr");
+        this.env = env;
+    }
+
+    public SingleMessageSentResponse sendAuthCode(CoolSMSDto coolSMSDto) {
+
+        Random random = new Random();
+        String sendNumber = env.getProperty("cool.sms.phone.number");
+        String authCode = String.valueOf(random.nextInt(8888)+1000); // 범위 : 1000 ~ 9999
+
+        Message message = new Message();
+        message.setFrom(sendNumber);
+        message.setTo(coolSMSDto.getPhoneNumber());
+        message.setText("notionable 회원가입 인증번호는 [" + authCode + "]입니다.");
+
+
+        SingleMessageSentResponse response = messageService.sendOne(new SingleMessageSendingRequest(message));
+
+        return response;
+    }
 }
