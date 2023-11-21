eb44146fddeaf260f4dabeb8d46f6fb59dd5324e
seokhee <skdlzl753@naver.com>
2023-11-21 16:48:19 +0900

    docs: scripts/stop.sh fix

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -10,7 +10,7 @@ TIME_NOW=$(date +%c)
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f "$JAR_FILE")
 
-echo "current pid: $CURRENT_PID"
+echo "current pid: $CURRENT_PID" >> "$DEPLOY_LOG"
 
 # 프로세스가 켜져 있으면 종료
 if [ -z "$CURRENT_PID" ]; then
