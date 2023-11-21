573363b3bfef520e4640fd1551c0fa8a76cb39d8
seokhee <skdlzl753@naver.com>
2023-11-21 16:15:36 +0900

    feat: user sms auth controller, Exception log

--- src/main/java/com/e1i6/notionable/domain/usersmsauth/controller/UserSMSAuthController.java
+++ src/main/java/com/e1i6/notionable/domain/usersmsauth/controller/UserSMSAuthController.java
@@ -1,33 +1,37 @@
 package com.e1i6.notionable.domain.usersmsauth.controller;
 
-import com.e1i6.notionable.domain.user.data.dto.response.EmailLoginResDto;
-import com.e1i6.notionable.domain.usersmsauth.dto.UserSMSAuthDto;
-import com.e1i6.notionable.domain.usersmsauth.service.UserSMSAuthService;
-import com.e1i6.notionable.global.common.response.BaseResponse;
-import com.e1i6.notionable.global.common.response.ResponseCode;
-import com.e1i6.notionable.global.common.response.ResponseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.e1i6.notionable.domain.usersmsauth.dto.UserSMSAuthDto;
+import com.e1i6.notionable.domain.usersmsauth.service.UserSMSAuthService;
+import com.e1i6.notionable.global.common.response.BaseResponse;
+import com.e1i6.notionable.global.common.response.ResponseCode;
+import com.e1i6.notionable.global.common.response.ResponseException;
+
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class UserSMSAuthController {
 
-    @Autowired
-    private UserSMSAuthService userSMSAuthService;
+	@Autowired
+	private UserSMSAuthService userSMSAuthService;
 
-    // 문자 인증번호 전송 API
-    @PostMapping("/signup/send-code")
-    public BaseResponse<?> sendAuthCode(@RequestBody UserSMSAuthDto userSMSAuthDto) {
+	// 문자 인증번호 전송 API
+	@PostMapping("/signup/send-code")
+	public BaseResponse<?> sendAuthCode(@RequestBody UserSMSAuthDto userSMSAuthDto) {
 
-        try {
-            UserSMSAuthDto resultSMSAuthDto = userSMSAuthService.sendAuthCode(userSMSAuthDto);
-            return new BaseResponse<>(resultSMSAuthDto);
-        } catch (ResponseException e) {
-            return new BaseResponse<>(e.getErrorCode());
-        } catch (Exception e) {
-            return new BaseResponse<>(ResponseCode.INTERNAL_SERVER_ERROR);
-        }
-    }
+		try {
+			UserSMSAuthDto resultSMSAuthDto = userSMSAuthService.sendAuthCode(userSMSAuthDto);
+			return new BaseResponse<>(resultSMSAuthDto);
+		} catch (ResponseException e) {
+			return new BaseResponse<>(e.getErrorCode());
+		} catch (Exception e) {
+			log.info("error msg: {}", e.getMessage());
+			return new BaseResponse<>(ResponseCode.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
