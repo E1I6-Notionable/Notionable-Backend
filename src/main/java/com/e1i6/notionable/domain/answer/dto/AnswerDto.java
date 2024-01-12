049b7e6c75c7459ae4a80598d2092ae8a87c8e8b
bbangjun <lucid9dawn@gmail.com>
2024-01-12 18:24:21 +0900

    feat(AnswerDto.java): Dto 필드 정의 및 entity to dto 메서드 추가

--- src/main/java/com/e1i6/notionable/domain/answer/dto/AnswerDto.java
+++ src/main/java/com/e1i6/notionable/domain/answer/dto/AnswerDto.java
@@ -1,6 +1,8 @@
 package com.e1i6.notionable.domain.answer.dto;
 
+import com.e1i6.notionable.domain.answer.entity.Answer;
 import com.e1i6.notionable.domain.creator.entity.Creator;
+import com.e1i6.notionable.domain.inquiry.dto.InquiryDto;
 import com.e1i6.notionable.domain.inquiry.entity.Inquiry;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,4 +24,19 @@ public class AnswerDto {
     private String fileUrl;
     private Inquiry inquiry;
     private Creator creator;
+
+    public static AnswerDto toAnswerDto(Answer answer) {
+        AnswerDto answerDto = new AnswerDto();
+
+        answerDto.setAnswer_id(answer.getAnswer_id());
+        answerDto.setTitle(answer.getTitle());
+        answerDto.setContent(answer.getContent());
+        answerDto.setCreatedAt(answer.getCreatedAt());
+        answerDto.setStatus(answer.getStatus());
+        answerDto.setFileUrl(answer.getFileUrl());
+        answerDto.setInquiry(answer.getInquiry());
+        answerDto.setCreator(answer.getCreator());
+
+        return answerDto;
+    }
 }
