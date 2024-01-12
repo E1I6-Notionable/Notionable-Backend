1c673caee5d116455a75c92220306997c388c2dc
bbangjun <lucid9dawn@gmail.com>
2024-01-12 17:18:58 +0900

    feat(Inquiry.java): user 테이블에 대한 N:1 매핑, answer 테이블에 대한 1:1 매핑 설정

--- src/main/java/com/e1i6/notionable/domain/inquiry/entity/Inquiry.java
+++ src/main/java/com/e1i6/notionable/domain/inquiry/entity/Inquiry.java
@@ -1,5 +1,6 @@
 package com.e1i6.notionable.domain.inquiry.entity;
 
+import com.e1i6.notionable.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,4 +25,13 @@ public class Inquiry {
     private String content;
     private LocalDateTime createdAt; // 문의 작성일
     private String status; // 답변유무 (Yes, No)
+
+    // 여러 개의 문의가 하나의 유저에게 속할 수 있으므로 N:1
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 하나의 문의에는 하나의 답변만 달릴 수 있으므로 1:1
+    @OneToOne(mappedBy = "inquiry", cascade = CascadeType.ALL)
+    private Answer answer;
 }
